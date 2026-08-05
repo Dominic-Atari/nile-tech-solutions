b9bf6cadfbc000dc3cb96112eeb47a621138bc7d
dominicobiala <dominico@anchovy.app>
2026-08-04 19:29:13 -0500

    Little edits

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -19,6 +19,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.textContent).toContain('NileTech');
+    expect(compiled.textContent).toContain('NileTechSolutions');
   });
 });
